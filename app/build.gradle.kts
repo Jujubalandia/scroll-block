355b41b89571a817b7cd080e29526b7385a9a108
vishal2376 <vishalsingh2376@gmail.com>
2024-06-12 19:34:58 +0530

    chore: modify package for debug

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,11 @@ android {
 				"proguard-rules.pro"
 			)
 		}
+
+		debug {
+			applicationIdSuffix = ".debug"
+			versionNameSuffix = "-debug"
+		}
 	}
 	compileOptions {
 		sourceCompatibility = JavaVersion.VERSION_1_8
