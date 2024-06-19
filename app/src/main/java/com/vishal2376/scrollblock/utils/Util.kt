40e8228ff187a6fa77f6383606ee0be3adca8dec
Vishal Singh <vishalsingh2376@gmail.com>
2024-06-19 22:01:38 +0530

    chore: impl openMail function

--- app/src/main/java/com/vishal2376/scrollblock/utils/Util.kt
+++ app/src/main/java/com/vishal2376/scrollblock/utils/Util.kt
@@ -1,5 +1,9 @@
 package com.vishal2376.scrollblock.utils
 
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import com.vishal2376.scrollblock.R
 import com.vishal2376.scrollblock.domain.model.AppUsage
 
 val instagramPackage = listOf(SupportedApps.Instagram.packageName)
@@ -12,10 +16,18 @@ val youtubePackage = listOf(
 )
 
 fun getAppScrollCount(
-	allAppUsage: List<AppUsage>,
-	packageName: List<String>
+	allAppUsage: List<AppUsage>, packageName: List<String>
 ): Int {
-	return allAppUsage
-		.filter { it.packageName in packageName }
-		.sumOf { it.scrollCount }
+	return allAppUsage.filter { it.packageName in packageName }.sumOf { it.scrollCount }
+}
+
+fun openMail(context: Context, title: String) {
+	val subject = "${context.getString(R.string.app_name)}: $title"
+	val uriBuilder = StringBuilder("mailto:" + Uri.encode(Constants.EMAIL))
+	uriBuilder.append("?subject=" + Uri.encode(subject))
+	val uriString = uriBuilder.toString()
+
+	val intentTitle = "Send $title"
+	val intent = Intent(Intent.ACTION_SENDTO, Uri.parse(uriString))
+	context.startActivity(Intent.createChooser(intent, intentTitle))
 }
