a69b5afc2865e58c2e47429efaa195078955e773
Vishal Singh <vishalsingh2376@gmail.com>
2024-06-21 20:21:35 +0530

    chore: change initial start destination based on permission

--- app/src/main/java/com/vishal2376/scrollblock/presentation/navigation/NavGraph.kt
+++ app/src/main/java/com/vishal2376/scrollblock/presentation/navigation/NavGraph.kt
@@ -3,6 +3,9 @@ package com.vishal2376.scrollblock.presentation.navigation
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -10,16 +13,27 @@ import com.vishal2376.scrollblock.presentation.home.HomeScreen
 import com.vishal2376.scrollblock.presentation.onboarding.PermissionScreen
 import com.vishal2376.scrollblock.presentation.onboarding.WelcomeScreen
 import com.vishal2376.scrollblock.presentation.viewmodels.MainViewModel
+import com.vishal2376.scrollblock.utils.isAccessibilityServiceEnabled
 
 
 @Composable
 fun NavGraph(viewModel: MainViewModel) {
+	val context = LocalContext.current
 	val navController = rememberNavController()
 
 	val allAppUsage by viewModel.appUsageList.collectAsState(initial = emptyList())
+	val isServiceEnabled by remember {
+		mutableStateOf(isAccessibilityServiceEnabled(context))
+	}
+
+	val startRoute = if (isServiceEnabled) {
+		Screen.HomeScreen.name
+	} else {
+		Screen.PermissionScreen.name
+	}
 
 	NavHost(
-		navController = navController, startDestination = Screen.WelcomeScreen.name
+		navController = navController, startDestination = startRoute
 	) {
 		composable(Screen.HomeScreen.name) {
 			HomeScreen(allAppUsage, onMainEvent = viewModel::onEvent, onNavigate = { route ->
@@ -30,7 +44,7 @@ fun NavGraph(viewModel: MainViewModel) {
 		composable(Screen.WelcomeScreen.name) {
 			WelcomeScreen(onNavigate = { route ->
 				navController.navigate(route = route) {
-					popUpTo(Screen.WelcomeScreen.name){
+					popUpTo(Screen.WelcomeScreen.name) {
 						inclusive = true
 					}
 				}
@@ -40,7 +54,7 @@ fun NavGraph(viewModel: MainViewModel) {
 		composable(Screen.PermissionScreen.name) {
 			PermissionScreen(onNavigate = { route ->
 				navController.navigate(route = route) {
-					popUpTo(Screen.PermissionScreen.name){
+					popUpTo(Screen.PermissionScreen.name) {
 						inclusive = true
 					}
 				}
