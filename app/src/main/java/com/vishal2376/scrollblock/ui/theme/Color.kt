d4bf11df14dd6fe8604560d7a927e148a2cbe066
Vishal Singh <vishalsingh2376@gmail.com>
2024-06-22 22:09:31 +0530

    chore: improve pie chart indicator colors

--- app/src/main/java/com/vishal2376/scrollblock/ui/theme/Color.kt
+++ app/src/main/java/com/vishal2376/scrollblock/ui/theme/Color.kt
@@ -13,8 +13,13 @@ val orange = Color(0xFFFF8C32)
 val green = Color(0xFF7ECA9C)
 val red = Color(0xFFFFA2A2)
 
+val instagram = Color(0xFFFF4F98)
+val youtube = Color(0xFFFA6B6B)
+val linkedin = Color(0xFF369BFF)
+val snapchat = Color(0xFFFFFD49)
+
 val white = Color(0xFFD3DEEB)
 val gray = Color(0xFF9E9E9E)
 
 
-val pieChartColors = listOf(blue, white, red, green)
+val pieChartColors = listOf(instagram, youtube, linkedin, snapchat)
