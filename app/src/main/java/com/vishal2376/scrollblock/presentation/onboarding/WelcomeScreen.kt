337538a7e613e7cce909033cf768b8017e5bdb5c
Vishal Singh <vishalsingh2376@gmail.com>
2024-06-23 13:03:52 +0530

    chore: add animation in welcome screen

--- app/src/main/java/com/vishal2376/scrollblock/presentation/onboarding/WelcomeScreen.kt
+++ app/src/main/java/com/vishal2376/scrollblock/presentation/onboarding/WelcomeScreen.kt
@@ -1,11 +1,17 @@
 package com.vishal2376.scrollblock.presentation.onboarding
 
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.spring
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -18,8 +24,14 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -32,9 +44,29 @@ import com.vishal2376.scrollblock.ui.theme.ScrollBlockTheme
 import com.vishal2376.scrollblock.ui.theme.blackGradient
 import com.vishal2376.scrollblock.ui.theme.blue
 import com.vishal2376.scrollblock.ui.theme.orange
+import kotlinx.coroutines.delay
 
 @Composable
 fun WelcomeScreen(onNavigate: (String) -> Unit) {
+
+	var animationPlayed by remember { mutableStateOf(false) }
+
+	val textAlpha by animateFloatAsState(
+		targetValue = if (animationPlayed) 1f else 0f,
+		label = "",
+		animationSpec = tween(delayMillis = 200, durationMillis = 1000)
+	)
+
+	val imgSize by animateDpAsState(
+		targetValue = if (animationPlayed) 300.dp else 100.dp, animationSpec = spring(
+			dampingRatio = Spring.DampingRatioMediumBouncy, stiffness = Spring.StiffnessLow
+		), label = ""
+	)
+
+	LaunchedEffect(Unit) {
+		delay(100)
+		animationPlayed = true
+	}
 
 	Scaffold(floatingActionButton = {
 		FloatingActionButton(
@@ -63,17 +95,24 @@ fun WelcomeScreen(onNavigate: (String) -> Unit) {
 			Spacer(modifier = Modifier.height(40.dp))
 
 			Image(
-				modifier = Modifier.size(300.dp),
+				modifier = Modifier.size(imgSize),
 				painter = painterResource(id = R.drawable.onboarding),
 				contentDescription = null
 			)
 			Spacer(modifier = Modifier.height(40.dp))
-			Text(text = "Welcome to", style = h2style)
-			Spacer(modifier = Modifier.height(24.dp))
 
-			Text(text = "ScrollBlock", style = titleStyle, color = blue)
-			Spacer(modifier = Modifier.height(8.dp))
-			Text(text = "Regain Control  Boost Productivity", style = descriptionStyle)
+			Column(
+				modifier = Modifier
+					.fillMaxWidth()
+					.graphicsLayer { alpha = textAlpha },
+				horizontalAlignment = Alignment.CenterHorizontally
+			) {
+				Text(text = "Welcome to", style = h2style)
+				Spacer(modifier = Modifier.height(24.dp))
+				Text(text = "ScrollBlock", style = titleStyle, color = blue)
+				Spacer(modifier = Modifier.height(8.dp))
+				Text(text = "Regain Control  Boost Productivity", style = descriptionStyle)
+			}
 		}
 	}
 }
